e2f0a304d649cd331734edec77ccfff2dcfb4f8b
Leon-1c <52901534+Leon-1c@users.noreply.github.com>
2021-06-07 23:26:40 +0800

    Add files via upload

--- 大数据怎么存_文件同步器/addTag.py
+++ 大数据怎么存_文件同步器/addTag.py
@@ -6,11 +6,11 @@ import time
 def lambda_handler(event, context):
     evtdata = event["Records"][0]["s3"]
     bucket = evtdata["bucket"]["name"]
-    endpoint_url = "http://10.16.0.1:81"
+    endpoint_url = ""
 
     s3Client = boto3.client("s3",
-                            aws_access_key_id="D3F24E7956EFF2C9152B",
-                            aws_secret_access_key="W0M4NDVDMTc2QkQwRTc0OTAxQTgzMTMxMTM4NUVG",
+                            aws_access_key_id="",
+                            aws_secret_access_key="",
                             endpoint_url=endpoint_url
                             )
 
