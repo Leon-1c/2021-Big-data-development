24a6cb477e87262b0fa41e3de779ac66ed2b1815
Leon-1c <52901534+Leon-1c@users.noreply.github.com>
2021-06-14 20:19:27 +0800

    Update jdbc.py

--- 大数据离线计算_Sql查询器/SparkSql/jdbc.py
+++ 大数据离线计算_Sql查询器/SparkSql/jdbc.py
@@ -1,13 +1,6 @@
 import os
 import jaydebeapi
 
-url = 'jdbc:hive2://bigdata129.depts.bingosoft.net:22129/user08_db'
-user = 'user08'
-password = 'pass@bingo8'
-dirver = 'org.apache.hive.jdbc.HiveDriver'
-
-# sql = 'show tables'
-
 
 def get_jar():
     DIR = './lib/'
@@ -33,11 +26,3 @@ def search(user, password, url, sql):
     conn.close()
     return [i[0] for i in tab], result
 
-
-if __name__ == '__main__':
-    user = ''
-    password = ''
-    a,b = search(user, password, url, 'show tables')
-    print(a)
-    for i in b:
-        print(i)
